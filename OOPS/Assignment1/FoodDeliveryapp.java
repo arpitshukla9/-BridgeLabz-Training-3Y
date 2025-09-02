5045e0f4627ee453eb0e7f4def9c35c1acd9faec
arpitshukla9 <arpitshukla0901@gmail.com>
2025-09-02 17:06:59 +0530

    [Arpit] Add: FoodDeliveryApp Class

---
+++ OOPS/Assignment1/FoodDeliveryapp.java
@@ -0,0 +1,32 @@
+package OOPS.Assignment1;
+
+public class FoodDeliveryApp {
+
+    private String name;
+    private String location;
+    private String[] foodItems;
+
+    public FoodDeliveryApp(String name, String location, String[] foodItems) {
+        this.name = name;
+        this.location = location;
+        this.foodItems = foodItems;
+    }
+
+    public void displayDetails() {
+        System.out.println("Restaurant Name: " + name);
+        System.out.println("Location: " + location);
+        System.out.println("Food Items: ");
+        for (String item : foodItems) {
+            System.out.println("- " + item);
+        }
+    }
+
+    public boolean isFoodAvailable(String food) {
+        for (String item : foodItems) {
+            if (item.equalsIgnoreCase(food)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ OOPS/Assignment1/FoodObj.java
@@ -0,0 +1,23 @@
+package OOPS.Assignment1;
+
+
+public class FoodObj {
+
+    public static void main(String[] args) {
+        
+        String[] foodItems1 = {"Pizza", "Pasta", "Burger"};
+        String[] foodItems2 = {"Sushi", "Ramen", "Tempura"};
+
+        FoodDeliveryApp restaurant1 = new FoodDeliveryApp("Italian Delight", "Downtown", foodItems1);
+        FoodDeliveryApp restaurant2 = new FoodDeliveryApp("Tokyo Treats", "Uptown", foodItems2);
+
+        System.out.println("=== Restaurant 1 ===");
+        restaurant1.displayDetails();
+        System.out.println("\n=== Restaurant 2 ===");
+        restaurant2.displayDetails();
+
+        System.out.println("\nChecking Food Availability:");
+        System.out.println("Is Pasta available in Italian Delight? " + restaurant1.isFoodAvailable("Pasta"));
+        System.out.println("Is Sushi available in Italian Delight? " + restaurant1.isFoodAvailable("Sushi"));
+    }
+}
